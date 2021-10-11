dc9fe9dcee9c42706d444a71d6d842ee1c57d25e
Jeff Mesnil <jmesnil@redhat.com>
2021-10-11 14:48:05 +0200

    kinda functional maven resolver
    Signed-off-by: Jeff Mesnil <jmesnil@redhat.com>

--- app/src/main/java/org/wildfly/channel/app/SimpleMavenVersionResolver.java
+++ app/src/main/java/org/wildfly/channel/app/SimpleMavenVersionResolver.java
@@ -21,9 +21,28 @@
  */
 package org.wildfly.channel.app;
 
+import static java.util.Optional.empty;
+
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
+import org.apache.maven.repository.internal.MavenRepositorySystemUtils;
+import org.eclipse.aether.DefaultRepositorySystemSession;
+import org.eclipse.aether.RepositorySystem;
+import org.eclipse.aether.RepositorySystemSession;
+import org.eclipse.aether.artifact.Artifact;
+import org.eclipse.aether.artifact.DefaultArtifact;
+import org.eclipse.aether.connector.basic.BasicRepositoryConnectorFactory;
+import org.eclipse.aether.impl.DefaultServiceLocator;
+import org.eclipse.aether.repository.LocalRepository;
+import org.eclipse.aether.repository.RemoteRepository;
+import org.eclipse.aether.resolution.ArtifactRequest;
+import org.eclipse.aether.resolution.ArtifactResolutionException;
+import org.eclipse.aether.resolution.ArtifactResult;
+import org.eclipse.aether.spi.connector.RepositoryConnectorFactory;
+import org.eclipse.aether.spi.connector.transport.TransporterFactory;
+import org.eclipse.aether.transport.http.HttpTransporterFactory;
 import org.wildfly.channel.MavenRepository;
 import org.wildfly.channel.spi.MavenVersionResolver;
 import org.wildfly.channel.version.VersionComparator;
@@ -31,6 +50,56 @@ import org.wildfly.channel.version.VersionComparator;
 public class SimpleMavenVersionResolver implements MavenVersionResolver {
     @Override
     public Optional<String> resolve(String groupId, String artifactId, List<MavenRepository> mavenRepositories, boolean resolveLocalCache, VersionComparator versionComparator) {
-        return Optional.of("1.0.0.Final");
+
+        List<RemoteRepository> remoteRepositories = mavenRepositories.stream().map(r -> newRemoteRepository(r)).collect(Collectors.toList());
+
+        RepositorySystem system = newRepositorySystem();
+        RepositorySystemSession session = newRepositorySystemSession(system);
+
+        Artifact artifact = new DefaultArtifact("org.eclipse.aether:aether-util:1.0.0.v20140518");
+        ArtifactRequest artifactRequest = new ArtifactRequest();
+        artifactRequest.setArtifact(artifact);
+        artifactRequest.setRepositories(remoteRepositories);
+
+        ArtifactResult artifactResult = null;
+        try {
+            artifactResult = system.resolveArtifact(session, artifactRequest);
+            artifact = artifactResult.getArtifact();
+
+            System.out.println(artifact + " resolved to  " + artifact.getFile());
+            return Optional.of("1.0.0.Final");
+        } catch (ArtifactResolutionException e) {
+            e.printStackTrace();
+            return empty();
+        }
+
+    }
+
+    public static RepositorySystem newRepositorySystem() {
+        DefaultServiceLocator locator = MavenRepositorySystemUtils.newServiceLocator();
+        locator.addService(RepositoryConnectorFactory.class, BasicRepositoryConnectorFactory.class);
+        locator.addService(TransporterFactory.class, HttpTransporterFactory.class);
+        locator.setErrorHandler(new DefaultServiceLocator.ErrorHandler() {
+            @Override
+            public void serviceCreationFailed(Class<?> type, Class<?> impl, Throwable exception) {
+                exception.printStackTrace();
+            }
+        });
+        return locator.getService(RepositorySystem.class);
+    }
+
+    public static DefaultRepositorySystemSession newRepositorySystemSession(RepositorySystem system) {
+        DefaultRepositorySystemSession session = MavenRepositorySystemUtils.newSession();
+
+        LocalRepository localRepo = new LocalRepository("target/local-repo");
+        session.setLocalRepositoryManager(system.newLocalRepositoryManager(session, localRepo));
+        // uncomment to generate dirty trees
+        // session.setDependencyGraphTransformer( null );
+
+        return session;
+    }
+
+    private static RemoteRepository newRemoteRepository(MavenRepository mavenRepository) {
+        return new RemoteRepository.Builder(mavenRepository.getId(), "default", mavenRepository.getUrl().toExternalForm()).build();
     }
 }
