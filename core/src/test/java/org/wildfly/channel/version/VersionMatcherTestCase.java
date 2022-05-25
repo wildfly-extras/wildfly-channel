fc9febf1211ced5e327c82a43b3ad96bb18bd8b8
Jeff Mesnil <jmesnil@redhat.com>
2022-05-25 11:53:26 +0200

    [#46] Finding the latest version
    * update doc to specify how the latest version is determined
    * add tests to check the VersionMatcher
    
    Signed-off-by: Jeff Mesnil <jmesnil@redhat.com>
    
    This fixes #46
    This fixes #3

--- core/src/test/java/org/wildfly/channel/version/VersionMatcherTestCase.java
+++ core/src/test/java/org/wildfly/channel/version/VersionMatcherTestCase.java
@@ -16,16 +16,82 @@
  */
 package org.wildfly.channel.version;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.wildfly.channel.version.VersionMatcher.getLatestVersion;
+
 import java.util.Set;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class VersionMatcherTestCase {
 
     @Test
-    public void testLatestVersion() {
-        Assertions.assertEquals("3", VersionMatcher.getLatestVersion(Set.of("1", "3", "2")).get());
+    public void test1() {
+        assertLatestVersion("3",
+                "1", "3", "2");
+    }
 
+    @Test
+    public void test2() {
+        assertLatestVersion("202109080827",
+                "1", "202009080827", "202109080827");
+    }
+
+    @Test
+    public void test3() {
+        assertLatestVersion("1.1.4",
+                "1.0.0", "1.0.2", "1.1.2", "1.1.4");
+    }
+
+    @Test
+    public void test4() {
+        assertLatestVersion("1.2",
+                "1.0", "1.0.2", "1.1", "1.2");
+    }
+
+    @Test
+    public void test5() {
+        assertLatestVersion("Z",
+                "A", "B", "C", "Z");
+    }
+
+    @Test
+    public void test6() {
+        assertLatestVersion("1.0.1.GA",
+                "1.0.GA", "1.0.1.GA");
+    }
+
+    @Test
+    public void test7() {
+        assertLatestVersion("1.0.0.Beta",
+                "1.0.0.Alpha", "1.0.0.Beta");
+    }
+
+    @Test
+    public void test8() {
+        assertLatestVersion("1:1",
+                "0:1", "1:1");
+    }
+
+    @Test
+    public void test9() {
+        assertLatestVersion("2.1.9.redhat-1",
+                "2.1.9.redhat-001", "2.1.9.redhat-1");
+    }
+
+    @Test
+    public void test10() {
+        assertLatestVersion("2.1.9.redhat-2",
+                "2.1.9.redhat-001", "2.1.9.redhat-2");
+    }
+
+    @Test
+    public void test11() {
+        assertLatestVersion("2.1.9.redhat-002",
+                "2.1.9.redhat-1", "2.1.9.redhat-002");
+    }
+
+    private static void assertLatestVersion(String expectedLatest, String... versions) {
+        assertEquals(expectedLatest, getLatestVersion(Set.of(versions)).get());
     }
 }
