8172af2d058b32d07889cd7f3e7fe1abd3cfac54
Bartosz Spyrko-Smietanko <bspyrkos@redhat.com>
2023-02-03 12:06:21 +0100

    Serialize manifest coordinate as 'manifest'

--- core/src/test/java/org/wildfly/channel/ChannelMapperTestCase.java
+++ core/src/test/java/org/wildfly/channel/ChannelMapperTestCase.java
@@ -17,7 +17,9 @@
 package org.wildfly.channel;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.net.URL;
 import java.util.List;
@@ -38,6 +40,7 @@ public class ChannelMapperTestCase {
 
         final Channel channel1 = ChannelMapper.fromString(yaml).get(0);
         assertEquals(Vendor.Support.COMMUNITY, channel1.getVendor().getSupport());
+        assertFalse(yaml.contains("manifestCoordinate:"));
     }
 
     @Test
