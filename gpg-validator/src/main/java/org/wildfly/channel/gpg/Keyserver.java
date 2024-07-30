31e87793297c922e29de464bff69610717f4112c
Bartosz Spyrko-Smietanko <bspyrkos@redhat.com>
2024-07-30 17:40:32 +0200

    Add implementation of SignatureValidator

---
+++ gpg-validator/src/main/java/org/wildfly/channel/gpg/Keyserver.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright 2024 Red Hat, Inc. and/or its affiliates
+ * and other contributors as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.wildfly.channel.gpg;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.bouncycastle.openpgp.PGPException;
+import org.bouncycastle.openpgp.PGPPublicKeyRing;
+import org.bouncycastle.openpgp.PGPPublicKeyRingCollection;
+import org.bouncycastle.openpgp.PGPUtil;
+import org.bouncycastle.openpgp.operator.bc.BcKeyFingerprintCalculator;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.math.BigInteger;
+import java.net.URI;
+import java.net.URL;
+import java.util.List;
+
+public class Keyserver {
+
+    private static final String LOOKUP_PATH = "/pks/lookup";
+
+    private final List<URL> servers;
+
+    public Keyserver(List<URL> serverUrls) {
+        this.servers = serverUrls;
+    }
+
+    public PGPPublicKeyRing downloadKey(String keyID) throws PGPException, IOException {
+        for (URL server : servers) {
+            final PGPPublicKeyRing publicKey = tryDownloadKey(server, keyID);
+            if (publicKey != null) {
+                return publicKey;
+            }
+        }
+        return null;
+    }
+
+    private PGPPublicKeyRing tryDownloadKey(URL serverUrl, String keyID) throws IOException, PGPException {
+        final String protocol;
+        if (serverUrl.getProtocol().equals("hkps")) {
+            protocol = "https";
+        } else if (serverUrl.getProtocol().equals("hkp")) {
+            protocol = "http";
+        } else {
+            protocol = serverUrl.getProtocol();
+        }
+
+        final String host = serverUrl.getHost();
+        final int port = serverUrl.getPort();
+        final String path = serverUrl.getPath();
+
+        final URI keyUri = URI.create(protocol + "://" + host + ":" + port + "/" + path + LOOKUP_PATH + "?" + getQueryStringForGetKey(keyID));
+
+        final HttpUriRequest request = new HttpGet(keyUri);
+
+        try (final CloseableHttpClient client = HttpClientBuilder.create().build();
+             final CloseableHttpResponse response = client.execute(request)) {
+            if (response.getStatusLine().getStatusCode() == 200) {
+
+                final HttpEntity responseEntity = response.getEntity();
+                try (InputStream inputStream = responseEntity.getContent()) {
+                    final InputStream keyIn = PGPUtil.getDecoderStream(inputStream);
+                    final PGPPublicKeyRingCollection pgpRing = new PGPPublicKeyRingCollection(keyIn, new BcKeyFingerprintCalculator());
+                    final BigInteger bi = new BigInteger(keyID, 16);
+                    return pgpRing.getPublicKeyRing(bi.longValue());
+                }
+            } else {
+                return null;
+            }
+        }
+    }
+
+    private static String getQueryStringForGetKey(String keyID) {
+        return String.format("op=get&options=mr&search=0x%s", keyID);
+    }
+}
